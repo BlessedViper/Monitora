82a1cda2754403b87eb33196df38f594658a5488
BlessedViper <94268067+BlessedViper@users.noreply.github.com>
2022-03-04 14:07:00 -0300

    Update monitoraRecursos.ps1
    Ajuste no código para formatação do resultado final

--- monitoraRecursos.ps1
+++ monitoraRecursos.ps1
@@ -7,16 +7,12 @@ $funcProcessor = $ObjectProcessor.LoadPercentage
 $ObjectDisk = Get-WmiObject Win32_logicaldisk
 $numberofDisk= $ObjectDisk.Count
 
-Write-Host "Percentual de memória RAM utilizada: " $funcMemory"%"
-Write-Host "Percentual de usabilidade do processador: " $funcProcessor"%"
-
 for ($i=0; $i -lt $numberofDisk; $i++){
-$getfreespace = $ObjectDisk.FreeSpace | Select-Object -index $i
-$getsize = $ObjectDisk.Size | Select-Object -index $i
-$funcDisk = ((($getsize - $getfreespace)*100)/$getsize)
-$funcDisk | Out-File C:\logs\disk$i.log
-Write-Host "Percentual de espaço alocado no disco" $i": "$funcDisk"%"
+    $getfreespace = $ObjectDisk.FreeSpace | Select-Object -index $i
+    $getsize = $ObjectDisk.Size | Select-Object -index $i
+    $funcDisk = ((($getsize - $getfreespace)*100)/$getsize)
+    $funcDisk.ToString("N2") | Out-File C:\logs\disk$i.log
 }
 
-$funcMemory | out-file C:\logs\memory.log
+$funcMemory.ToString("N2") | out-file C:\logs\memory.log
 $funcProcessor | Out-File C:\logs\processor.log
